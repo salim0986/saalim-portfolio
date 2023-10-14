e7184a54aab8e3c207951d694ac8f3e0a20d7c25
Mohd Saalim <salimansari0986@gmail.com>
2023-10-14 18:44:41 +0530

    Fixed Some Errors

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -37,10 +37,12 @@ const Services = () => {
     service4: {
       initial: {
         opacity: 0,
-        x: "100%",
+        y: "50%",
+        x: "10%",
       },
       whileInView: {
         opacity: 1,
+        y: 0,
         x: 0,
       },
     },
